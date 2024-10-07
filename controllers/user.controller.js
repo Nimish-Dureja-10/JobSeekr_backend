b190cf8235a659090e650b58738f9c03d1bac44f
Nimish-Dureja-10 <durejanimish14@gmail.com>
2024-10-07 12:45:27 +0530

    .

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -89,8 +89,7 @@ export const login = async (req,res) => {
             profile:user.profile
         }
 
-        return res.status(200).cookie("token",token,{maxAge:3*24*60*60*1000,httpsOnly:true,sameSite:'strict'})
-        .json({success:true,user,message:`Welcome back ${user.fullname}`});
+        return res.status(200).cookie("token",token,{maxAge:3*24*60*60*1000,httpsOnly:true,sameSite:'strict'}).json({success:true,user,message:`Welcome back ${user.fullname}`});
     }catch(error) {
         console.log(error);
     }
